acf333d0856fb52eac047589dd50f7d996f891e1
Grace <njugunagraceewambui@gmail.com>
2023-06-16 23:56:51 +0300

    fetched todos api

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-
 const userContainer = document.getElementById('taskList');
-const addTaskForm = document.getElementById('addTaskForm');
 const successMessage = document.getElementById('successMessage');
+const addBtn = document.getElementById('addBtn');
+const deleteBtn = document.getElementById('deleteBtn');
+const userIdInput = document.getElementById('userIdInput');
 
 const getTodoById = async (userId) => {
   try {
@@ -21,26 +22,31 @@ const displayTodo = async (userId) => {
     const userName = document.createElement('input');
     const checkbox = document.createElement('input');
     const deleteBtn = document.createElement('button');
-    const gap = document.createElement('span');
+
     checkbox.type = 'checkbox';
     checkbox.checked = todo.completed;
     userName.value = todo.todo;
-    userName.classList.add('form-control');
-    checkbox.addEventListener('change', () => {
-      userName.classList.toggle('completed', checkbox.checked);
+
+    userName.addEventListener('change', () => {
+      if (checkbox.checked) {
+        userName.classList.add('completed');
+      } else {
+        userName.classList.remove('completed');
+      }
     });
+
     deleteBtn.textContent = 'Delete';
-    deleteBtn.addEventListener('click', async () => {
-      await deleteTodoById(userId);
+    deleteBtn.addEventListener('click', () => {
+      deleteTodoById(userId);
       li.remove();
     });
-    gap.style.marginRight = '10px';
+
     li.appendChild(checkbox);
     li.appendChild(userName);
     li.appendChild(deleteBtn);
-    li.setAttribute('data-key', userId);
+    li.setAttribute('key', userId);
     li.classList.add('task');
-    userContainer.prepend(li); // Add new todo at the top
+    userContainer.appendChild(li);
   } else {
     console.log(`Todo with user ID ${userId} not found.`);
   }
@@ -60,197 +66,17 @@ const deleteTodoById = async (userId) => {
 };
 
 const deleteTasks = () => {
-  const tasks = document.getElementsByClassName('task');
-  while (tasks.length > 0) {
-    tasks[0].remove();
-  }
+  userContainer.innerHTML = '';
 };
 
-addTaskForm.addEventListener('submit', async (e) => {
-  e.preventDefault();
-
-  const userId = parseInt(document.getElementById('userIdInput').value);
-  const task = document.getElementById('taskInput').value;
-
+addBtn.addEventListener('click', () => {
+  const userId = parseInt(userIdInput.value);
   if (!isNaN(userId)) {
-    await displayTodo(userId);
+    displayTodo(userId);
     successMessage.textContent = 'Todo added successfully.';
   } else {
     successMessage.textContent = 'Please enter a valid User ID.';
   }
-
-  document.getElementById('userIdInput').value = '';
-  document.getElementById('taskInput').value = '';
 });
 
-document.getElementById('deleteButton').addEventListener('click', deleteTasks);
-
-
-
-
-
-
-//  const todoCont = document.getElementById('taskList');
-
-
-//  const  getTodos=()=>{
-//   return fetch('https://dummyjson.com/todos?limit=1')
-//  .then(response=>response.json())
-//   .then(response=>response)
-//   .catch(error=>error)
-
-// ;
-// }
-// const displayTodos= async()=>{
-//   const todos=await getTodos();
-
-//   todos.map(x=>{
-//     let div = document.createElement('div');
-//     let name = document.createElement('p');
-//     let completed = document.createElement('p');
-    
-
-//     name.innerHTML = x.todo;
-//     completed.innerHTML= x.completed;
-
-//     div.appendChild(name);
-//     div.appendChild(completed);
-
-//     div.setAttribute('key',x.id);
-//     div.setAttribute('class','todo');
-//     todoCont.appendChild(div);
-
-
-//   });
-// }
-// displayTodos();
-
-
-
-
-
-
-
-// // Store tasks in an array
-// let tasks = [];
-
-// document.getElementById('addTaskForm').addEventListener('submit', async function(event){
-//   event.preventDefault();
-
-//   let todo = document.getElementById('todo').value;
-//   let completed = document.getElementById('completed').value;
-
-//   let data={
-//     todo:todo,
-//     completed:completed
-//   };
-//   console.log(data);
-
-
-//   let result= await fetch('https://dummyjson.com/todos/user/5' , )
-       
-  
-     
-
-// })
-
-
-
-
-
-
-// async function fetchTodos() {
-//   try {
-//     const response = await fetch('https://dummyjson.com/todos/user/5');
-//     const data = await response.json();
-//     tasks = data;
-//     displayTasks();
-//   } catch (error) {
-//     console.error('Error fetching todos:', error);
-//   }
-// }
-
-// Function to add a new task
-// function addTask(event) {
-//   event.preventDefault();
-
-//   // Get user input values
-//   const userId = document.getElementById('userId').value;
-//   const task = document.getElementById('task').value;
-
-//   // Create a new task object
-//   const newTask = { userId, task, completed: false };
-
-//   // Add the new task at the beginning of the tasks array
-//   tasks.unshift(newTask);
-
-//   // Clear the form fields
-//   document.getElementById('userId').value = '';
-//   document.getElementById('task').value = '';
-
-//   // Update the task list
-//   displayTasks();
-// }
-
-// // Function to complete a task
-// function completeTask(index) {
-//   // Toggle the completed status of the task
-//   tasks[index].completed = !tasks[index].completed;
-
-//   // Update the task list
-//   displayTasks();
-// }
-
-// // Function to delete a task
-// function deleteTask(index) {
-//   // Remove the task from the tasks array
-//   tasks.splice(index, 1);
-
-//   // Update the task list
-//   displayTasks();
-// }
-
-// // Function to display the tasks
-// function displayTasks() {
-//   const taskList = document.getElementById('taskList');
-
-//   // Clear the task list
-//   taskList.innerHTML = '';
-
-//   // Filter and display tasks based on user ID
-//   const userId = document.getElementById('userId').value;
-//   const filteredTasks = tasks.filter(task => task.userId == userId);
-
-//   // Add each task as an item in the task list
-//   filteredTasks.forEach((task, index) => {
-//     const taskItem = document.createElement('div');
-//     taskItem.classList.add('task-item');
-//     if (task.completed) {
-//       taskItem.classList.add('completed-task');
-//     }
-
-//     const taskContent = document.createElement('p');
-//     taskContent.textContent = task.task;
-
-//     const completeBtn = document.createElement('button');
-//     completeBtn.textContent = 'Complete';
-//     completeBtn.addEventListener('click', () => completeTask(index));
-
-//     const deleteBtn = document.createElement('button');
-//     deleteBtn.textContent = 'Delete';
-//     deleteBtn.classList.add('delete-btn');
-//     deleteBtn.addEventListener('click', () => deleteTask(index));
-
-//     taskItem.appendChild(taskContent);
-//     taskItem.appendChild(completeBtn);
-//     taskItem.appendChild(deleteBtn);
-
-//     taskList.appendChild(taskItem);
-//   });
-// }
-
-// // Add event listener to the form submit event
-// document.getElementById('addTaskForm').addEventListener('submit', addTask);
-
-// // Fetch todos when the page loads
-// fetchTodos();
+deleteBtn.addEventListener('click', deleteTasks);
